8236fae73755ace9203c93202d6a570045eafdc0
gaheul <rkgmf12302@naver.com>
2022-02-24 12:56:43 +0900

    반복문

--- JavaStudy_4452/src/a06_조건/IfElseEx2.java
+++ JavaStudy_4452/src/a06_조건/IfElseEx2.java
@@ -13,25 +13,50 @@ public class IfElseEx2 {
 		 * 두 수 중 큰 값은 20입니다.
 		 */
 		Scanner sc = new Scanner(System.in);
-		
 		int x = 0;
-		int y =0;
+		int y= 0;
 		int result = 0;
-		
-		System.out.print("두 수를 입력 해 주세요: ");
+		System.out.println("두 수를 입력해주세요: ");
 		x=sc.nextInt();
 		y=sc.nextInt();
 		
-		if(x == y) {
-			System.out.println("두 수가 같습니다.");
+		if(x>y) {
+			result=x;
 		}else {
-			if(x>y) {
-				result= x;
-			}else {
-				result=y;
-			}
-			System.out.println("두 수중 큰 값은 "+result+"입니다.");
+			result=y;
 		}
+		System.out.println("두 수 중 큰 값은 "+ result+"입니다.");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int x = 0;
+//		int y =0;
+//		int result = 0;
+//		
+//		System.out.print("두 수를 입력 해 주세요: ");
+//		x=sc.nextInt();
+//		y=sc.nextInt();
+//		
+//		if(x == y) {
+//			System.out.println("두 수가 같습니다.");
+//		}else {
+//			if(x>y) {
+//				result= x;
+//			}else {
+//				result=y;
+//			}
+//			System.out.println("두 수중 큰 값은 "+result+"입니다.");
+//		}
 		
 //		if(x>y) {
 //			result= x;
