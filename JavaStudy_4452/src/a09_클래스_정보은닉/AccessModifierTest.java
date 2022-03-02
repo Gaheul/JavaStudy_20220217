1f8f0eadd9511e1d1f165207e1a37c65b5e6d48b
ITPS <ITPS@gmail.com>
2022-03-02 12:57:53 +0900

    클래스 기초

---
+++ JavaStudy_4452/src/a09_클래스_정보은닉/AccessModifierTest.java
@@ -0,0 +1,30 @@
+package a09_클래스_정보은닉;
+
+import a09_클래스.Student;
+
+/**
+ * 접근지정자(AccessModifier)
+ * 
+ * default
+ * protected
+ * public
+ * private
+ * 
+ *
+ */
+
+public class AccessModifierTest {
+
+	public static void main(String[] args) {
+		AccessModifier accessModifier = new AccessModifier();
+		
+//		accessModifier.name ="김가흘" ;
+//		accessModifier.age =00 ;
+		
+		
+		Student student = new Student();
+		
+
+	}
+
+}
